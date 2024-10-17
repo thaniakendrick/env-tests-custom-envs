97df40ba899de9fe16ba408db1d337c079df03c3
Eevee Kendrick <thaniakendrick@users.noreply.github.com>
2024-10-16 21:40:21 -0500

    triggering a deployment for testing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Adding a small comment to trigger a deployment */}
       <p>this is {process.env.WHERE_AM_I}</p>
     </main>
   );
